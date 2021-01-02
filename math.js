88a08c5d39046e4d5d921152d09dd6387048789e
firozkabirroz <firozkabirroz@gmail.com>
2021-01-03 00:42:01 +0600

    math.js edited

--- math.js
+++ math.js
@@ -7,7 +7,7 @@ console.log(Math.abs(n))
 console.log(Math.floor(n))
 console.log(Math.ceil(n))
 console.log(Math.round(n))
-
+//  ok i am done with this 
 console.log(Math.pow(2,3))
 console.log(Math.pow(5,5))
 
