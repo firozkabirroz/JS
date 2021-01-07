ff60ec00539c8270d342d86bde827b88959efcde
firozkabirroz <firozkabirroz@gmail.com>
2021-01-07 22:51:55 +0600

    loop work done

--- loop.js
+++ loop.js
@@ -1,9 +1,20 @@
-var  sum = 0
+// var  sum = 0
 
-for (var i=2;i<100;i++){
-    if(i%2==0){
-        sum = sum +i;
-    }
+// for (var i=2;i<100;i++){
+//     if(i%2==0){
+//         sum = sum +i;
+//     }
     
+// }
+// console.log(sum)
+
+var n = 10
+for( var i=1;i<=10;i++){
+    var result =''
+    var finalResult =''
+    for(var j=1;j<=i;j++){
+        result +='*'
+        // finalResult = result+finalResult
+    }
+    console.log(result)
 }
-console.log(sum)
