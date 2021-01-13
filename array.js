ba0d46f3fe4e82bdfc2a4e276d43106e90befae4
firozkabirroz <firozkabirroz@gmail.com>
2021-01-13 11:18:51 +0600

    array first practice added

---
+++ array.js
@@ -0,0 +1,12 @@
+var arr = [1,4,3,5,6]
+
+arr[5]=6
+arr[10]=11
+
+console.log(arr)
+console.log(arr[3])
+arr[2]=23
+console.log(arr)
+console.log(arr.length)
+var arr2 = Array(1,2,3)
+console.log(arr2)
