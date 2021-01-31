a82961a6b3f3a6684184d3816b9e32922ce730b9
firozkabirroz <firozkabirroz@gmail.com>
2021-02-01 00:18:04 +0600

    code practice done

--- object.js
+++ object.js
@@ -1,4 +1,30 @@
-var obj = {}
-obj.x = 10
+// var obj = {}
+// obj.x = 10
 
-console.log(obj)
+// console.log(obj)
+
+var point = {
+    x: 10,
+    y: 20 
+}
+
+point.z=30
+
+console.log(point)
+
+var obj1 = Object()
+
+obj1.a= 12
+
+console.log(obj1)
+
+var obj2 = new Object()
+obj2.e = 40
+console.log(obj2)
+
+console.log(point.x)
+
+console.log(point.x + point.y)
+
+console.log(point['x'])
+
