ab11295fef3dc7ec7bf20ca55a289f49786150a5
firozkabirroz <firozkabirroz@gmail.com>
2021-02-04 23:50:24 +0600

    closure practice done

---
+++ closure.js
@@ -0,0 +1,15 @@
+// var b = 10
+// function a(){
+//     console.log(b)
+// }
+
+var b = 10
+function a(){
+    var x = 5
+    return function(){
+        console.log(x)
+    }
+}
+
+var abc = a()
+console.dir(abc)
