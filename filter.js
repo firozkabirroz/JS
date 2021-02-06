775cdc0b5863da7f7b36e6dabba2fcbb71c7386f
firozkabirroz <firozkabirroz@gmail.com>
2021-02-06 23:37:34 +0600

    filter chapter done

--- filter.js
+++ filter.js
@@ -1,6 +1,20 @@
 var arr = [1,2,3,4,5,6,7,8]
 
-var filteredData = arr.filter(function(value){
-    return value % 2 ==0
-})
-console.log(filteredData)
+// var filteredData = arr.filter(function(value){
+//     return value % 2 ==0
+// })
+// console.log(filteredData)
+
+function myFilter(arr,cb){
+    var newArr = []
+    for(var i =0;i<arr.length ;i++){
+        if(cb(arr[i],i,arr)){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+console.log(myFilter(arr,function(value){
+    return value %2 == 0
+}))
