12ac33e681fca65cd4296d412fc57cc4aae3a0c9
firozkabirroz <firozkabirroz@gmail.com>
2021-02-08 14:03:21 +0600

    return code done

---
+++ return.js
@@ -0,0 +1,19 @@
+function greet(msg){
+    function greetings(name){
+        return msg + ' ,' + name + '!'
+    }
+    return greetings
+
+}
+
+var gm = greet('Good Morning')
+var gn = greet("Good Night")
+var hello = greet('Hello')
+
+var msg = gm('Firoz Kabir')
+
+console.log(msg)
+console.log(gn('Twinkle cats'))
+console.log(msg)
+console.log(hello('Segufa Taranjum
+'))
