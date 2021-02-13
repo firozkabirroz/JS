b6d70d7b0834d7c97465f8c6ba6473e2aa3183d0
firozkabirroz <firozkabirroz@gmail.com>
2021-02-14 01:35:04 +0600

    contex practice code

---
+++ contex.js
@@ -0,0 +1,22 @@
+function a(){
+    b()
+    console.log('I am Function A')
+}
+
+function b(){
+    d()
+    console.log(' I am Function B')
+}
+function c(){
+    
+    console.log('i am function c')
+}
+function d(){
+    c()
+    console.log('I am function D')
+}
+
+var x = 100
+
+a()
+console.log(' I Am Global')
