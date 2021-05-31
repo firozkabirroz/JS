3e6c2d639287b3bb054c09fb7c78359e40d494ae
firozkabirroz <firozkabirroz@gmail.com>
2021-05-31 11:50:34 +0600

    this practice code

--- Object/index.js
+++ Object/index.js
@@ -1,16 +1,33 @@
 var rect = {
-    width: 100,
+    width: 1000,
     height: 50,
 
     draw: function(){
         console.log('i am a rectangle')
+       this.printProperties()
+    },
+    printProperties: function(){
         console.log('My width is '+ this.width)
         console.log('My height is '+ this.height)
     }
 }
 
-rect.draw()
+ rect.draw()
 
-rect.width=100
+// rect.width=100
 
-rect.draw()
+// rect.draw()
+
+function myFunc(){
+    console.log(this)
+    
+}
+new myFunc()
+
+var another ={
+    width: 34,
+    height: 54,
+    print: rect.printProperties
+}
+
+another.print()
